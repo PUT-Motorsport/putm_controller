0a9915d6eb901e591f3bb9a86e1d2bc6ca583506
KozAAAAA <mateusz-koza@outlook.com>
2024-03-27 10:59:34 +0100

    Perform clang format

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,7 +1,8 @@
 #include <cstdio>
-#include "putm_pm09_vcl/msg/setpoints.hpp"
+
 #include "putm_pm09_vcl/msg/detail/frontbox__struct.hpp"
 #include "putm_pm09_vcl/msg/frontbox.hpp"
+#include "putm_pm09_vcl/msg/setpoints.hpp"
 #include "rclcpp/rclcpp.hpp"
 
 using namespace std::chrono_literals;
@@ -15,45 +16,45 @@ constexpr int32_t INVERTER_MAX_NEGATIVE_TOURQE = -500;
 
 class InvertersDriver : public rclcpp::Node
 {
-  public:
-    InvertersDriver(): Node("inverters_river"), pedalPosition(0), modifiers{0.5,0.5}
-    {
-      setpointsPublisher = this->create_publisher<putm_pm09_vcl::msg::Setpoints>("setpoints", 10);
-      frontBoxSubscriber = this->create_subscription<putm_pm09_vcl::msg::Frontbox>("frontbox", 10, std::bind(&InvertersDriver::frontboxTopicCallback, this, _1));
-      mainLoopTimer = this->create_wall_timer(10ms, std::bind(&InvertersDriver::MainLoopCallback, this));
-    }
+public:
+  InvertersDriver() : Node("inverters_river"), pedalPosition(0), modifiers{0.5, 0.5}
+  {
+    setpointsPublisher = this->create_publisher<putm_pm09_vcl::msg::Setpoints>("setpoints", 10);
+    frontBoxSubscriber = this->create_subscription<putm_pm09_vcl::msg::Frontbox>(
+      "frontbox", 10, std::bind(&InvertersDriver::frontboxTopicCallback, this, _1));
+    mainLoopTimer =
+      this->create_wall_timer(10ms, std::bind(&InvertersDriver::MainLoopCallback, this));
+  }
 
-  private:
-    void MainLoopCallback()
-    {
-      /* continue only if we are in rtd */
-      auto tourqeSetpoints = putm_pm09_vcl::msg::Setpoints();
-      int32_t tourqe_front = INVERTER_MAX_POSITIVE_TOURQE * pedalPosition * modifiers.front;
-      int32_t tourqe_rear =  INVERTER_MAX_POSITIVE_TOURQE * pedalPosition * modifiers.rear;
-      tourqeSetpoints.tourqes[0] = tourqe_front;
-      tourqeSetpoints.tourqes[1] = tourqe_front;
-      tourqeSetpoints.tourqes[2] = tourqe_rear;
-      tourqeSetpoints.tourqes[3] = tourqe_rear;
+private:
+  void MainLoopCallback()
+  {
+    /* continue only if we are in rtd */
+    auto tourqeSetpoints = putm_pm09_vcl::msg::Setpoints();
+    int32_t tourqe_front = INVERTER_MAX_POSITIVE_TOURQE * pedalPosition * modifiers.front;
+    int32_t tourqe_rear = INVERTER_MAX_POSITIVE_TOURQE * pedalPosition * modifiers.rear;
+    tourqeSetpoints.tourqes[0] = tourqe_front;
+    tourqeSetpoints.tourqes[1] = tourqe_front;
+    tourqeSetpoints.tourqes[2] = tourqe_rear;
+    tourqeSetpoints.tourqes[3] = tourqe_rear;
 
-      setpointsPublisher->publish(tourqeSetpoints);
+    setpointsPublisher->publish(tourqeSetpoints);
+  }
+  void frontboxTopicCallback(const putm_pm09_vcl::msg::Frontbox msg)
+  {
+    pedalPosition = msg.pedal_position;
+  }
+  float pedalPosition;
+  bool rtd_state;
+  struct tourqeModifiers
+  {
+    float front;
+    float rear;
+  } modifiers;
 
-    }
-    void frontboxTopicCallback(const putm_pm09_vcl::msg::Frontbox msg)
-    {
-      pedalPosition = msg.pedal_position;
-
-    }
-    float pedalPosition;
-    bool rtd_state;
-    struct tourqeModifiers
-    {
-      float front;
-      float rear;
-    }modifiers;
-
-    rclcpp::TimerBase::SharedPtr mainLoopTimer;
-    rclcpp::Subscription<putm_pm09_vcl::msg::Frontbox>:: SharedPtr frontBoxSubscriber;
-    rclcpp::Publisher<putm_pm09_vcl::msg::Setpoints>::SharedPtr setpointsPublisher;
+  rclcpp::TimerBase::SharedPtr mainLoopTimer;
+  rclcpp::Subscription<putm_pm09_vcl::msg::Frontbox>::SharedPtr frontBoxSubscriber;
+  rclcpp::Publisher<putm_pm09_vcl::msg::Setpoints>::SharedPtr setpointsPublisher;
 };
 
 int main(int argc, char ** argv)
