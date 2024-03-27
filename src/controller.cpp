48550c47865aa806dc1d13728c5a38e4d381f9e2
KozAAAAA <mateusz-koza@outlook.com>
2024-03-27 14:43:47 +0100

    Restructure code to make it more readable, Change to snake case

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,66 +1,72 @@
-#include <cstdio>
-
 #include "putm_pm09_vcl/msg/detail/frontbox__struct.hpp"
 #include "putm_pm09_vcl/msg/frontbox.hpp"
 #include "putm_pm09_vcl/msg/setpoints.hpp"
 #include "rclcpp/rclcpp.hpp"
 
 using namespace std::chrono_literals;
+using namespace putm_pm09_vcl::msg;
+
 using std::placeholders::_1;
 
-constexpr int32_t INVERTER_MAX_POSITIVE_TOURQE = 500;
-constexpr int32_t INVERTER_MAX_NEGATIVE_TOURQE = -500;
-
-/* This example creates a subclass of Node and uses std::bind() to register a
-* member function as a callback from the timer. */
-
-class InvertersDriver : public rclcpp::Node
+class Controller : public rclcpp::Node
 {
 public:
-  InvertersDriver() : Node("inverters_river"), pedalPosition(0), modifiers{0.5, 0.5}
-  {
-    setpointsPublisher = this->create_publisher<putm_pm09_vcl::msg::Setpoints>("setpoints", 10);
-    frontBoxSubscriber = this->create_subscription<putm_pm09_vcl::msg::Frontbox>(
-      "frontbox", 10, std::bind(&InvertersDriver::frontboxTopicCallback, this, _1));
-    mainLoopTimer =
-      this->create_wall_timer(10ms, std::bind(&InvertersDriver::MainLoopCallback, this));
-  }
+  Controller();
 
 private:
-  void MainLoopCallback()
-  {
-    /* continue only if we are in rtd */
-    auto tourqeSetpoints = putm_pm09_vcl::msg::Setpoints();
-    int32_t tourqe_front = INVERTER_MAX_POSITIVE_TOURQE * pedalPosition * modifiers.front;
-    int32_t tourqe_rear = INVERTER_MAX_POSITIVE_TOURQE * pedalPosition * modifiers.rear;
-    tourqeSetpoints.tourqes[0] = tourqe_front;
-    tourqeSetpoints.tourqes[1] = tourqe_front;
-    tourqeSetpoints.tourqes[2] = tourqe_rear;
-    tourqeSetpoints.tourqes[3] = tourqe_rear;
+  static constexpr int32_t INVERTER_MAX_POSITIVE_TOURQE = 500;
+  static constexpr int32_t INVERTER_MAX_NEGATIVE_TOURQE = -500;
 
-    setpointsPublisher->publish(tourqeSetpoints);
-  }
-  void frontboxTopicCallback(const putm_pm09_vcl::msg::Frontbox msg)
-  {
-    pedalPosition = msg.pedal_position;
-  }
-  float pedalPosition;
+  float pedal_position;
   bool rtd_state;
+
   struct tourqeModifiers
   {
     float front;
     float rear;
   } modifiers;
 
-  rclcpp::TimerBase::SharedPtr mainLoopTimer;
-  rclcpp::Subscription<putm_pm09_vcl::msg::Frontbox>::SharedPtr frontBoxSubscriber;
-  rclcpp::Publisher<putm_pm09_vcl::msg::Setpoints>::SharedPtr setpointsPublisher;
+  rclcpp::TimerBase::SharedPtr main_loop_timer;
+  rclcpp::Subscription<Frontbox>::SharedPtr frontbox_subscriber;
+  rclcpp::Publisher<Setpoints>::SharedPtr setpoints_publisher;
+
+  void main_loop_callback();
+  void frontbox_topic_callback(const Frontbox msg);
 };
+
+Controller::Controller() : Node("controller"), pedal_position(0), modifiers{0.5, 0.5}
+{
+  setpoints_publisher = this->create_publisher<Setpoints>("setpoints", 10);
+  frontbox_subscriber = this->create_subscription<Frontbox>(
+    "frontbox", 10, std::bind(&Controller::frontbox_topic_callback, this, _1));
+  main_loop_timer = this->create_wall_timer(10ms, std::bind(&Controller::main_loop_callback, this));
+}
+
+void Controller::main_loop_callback()
+{
+  /* continue only if we are in rtd */
+  if (rtd_state) {
+    auto tourqe_setpoints = Setpoints();
+    int32_t tourqe_front = INVERTER_MAX_POSITIVE_TOURQE * pedal_position * modifiers.front;
+    int32_t tourqe_rear = INVERTER_MAX_POSITIVE_TOURQE * pedal_position * modifiers.rear;
+    tourqe_setpoints.tourqes[0] = tourqe_front;
+    tourqe_setpoints.tourqes[1] = tourqe_front;
+    tourqe_setpoints.tourqes[2] = tourqe_rear;
+    tourqe_setpoints.tourqes[3] = tourqe_rear;
+
+    setpoints_publisher->publish(tourqe_setpoints);
+  }
+}
+
+void Controller::frontbox_topic_callback(const Frontbox msg)
+{
+  pedal_position = msg.pedal_position;
+}
 
 int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<InvertersDriver>());
+  rclcpp::spin(std::make_shared<Controller>());
   rclcpp::shutdown();
   return 0;
 }
