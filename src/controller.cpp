a6cebe379eb1a97bcdfdaeff625807da01a04c53
Adam <Adam_Wasilewski_99@proton.me>
2024-07-06 17:13:54 +0200

    remove modifiers

--- src/controller.cpp
+++ src/controller.cpp
@@ -14,15 +14,6 @@ class Controller : public rclcpp::Node {
  private:
   static constexpr float INVERTER_MAX_POSITIVE_TORQUE = 9.8;
   static constexpr float INVERTER_MAX_NEGATIVE_TORQUE = -9.8;
-  
-  static constexpr float MAX_STEERING_WHEEL_POSITION = 270.0;
-
-  bool rtd_state = true;  // TODO: Implement
-
-  struct torqueModifiers {
-    float left;
-    float right;
-  } modifiers;
 
   rclcpp::Subscription<FrontboxDriverInput>::SharedPtr frontbox_driver_input_subscriber;
   rclcpp::Publisher<Setpoints>::SharedPtr setpoints_publisher;
@@ -30,7 +21,7 @@ class Controller : public rclcpp::Node {
   void frontbox_driver_input_topic_callback(const FrontboxDriverInput msg);
 };
 
-Controller::Controller() : Node("controller"), modifiers{1, 1} {
+Controller::Controller() : Node("controller"){
   setpoints_publisher = this->create_publisher<Setpoints>("putm_vcl/setpoints", 1);
   frontbox_driver_input_subscriber =
       this->create_subscription<FrontboxDriverInput>("putm_vcl/frontbox_driver_input", 1, std::bind(&Controller::frontbox_driver_input_topic_callback, this, _1));
@@ -38,24 +29,16 @@ Controller::Controller() : Node("controller"), modifiers{1, 1} {
 
 void Controller::frontbox_driver_input_topic_callback(const FrontboxDriverInput msg) {
   float scaled_pedal_position = ((float)(msg.pedal_position) / 500.0);
-  
-  float left_scaler = -(msg.steering_wheel_position - MAX_STEERING_WHEEL_POSITION) / MAX_STEERING_WHEEL_POSITION * 2;
-  float right_scaler = msg.steering_wheel_position / MAX_STEERING_WHEEL_POSITION * 2;
 
-  modifiers.left = left_scaler > 1 ? 1 : left_scaler + 0.4;
-  modifiers.right = right_scaler > 1 ? 1 : right_scaler + 0.4;
-
-  if (rtd_state) {
-    auto torque_setpoints = Setpoints();
-    int32_t torque_left = INVERTER_MAX_POSITIVE_TORQUE * scaled_pedal_position * 10.0 * modifiers.left;
-    int32_t torque_right = INVERTER_MAX_POSITIVE_TORQUE * scaled_pedal_position * 10.0 * modifiers.right;
-    torque_setpoints.torques[0] = torque_left;
-    torque_setpoints.torques[1] = torque_right;
-    torque_setpoints.torques[2] = torque_left;
-    torque_setpoints.torques[3] = torque_right;
+  auto torque_setpoints = Setpoints();
+  int32_t torque_left = INVERTER_MAX_POSITIVE_TORQUE * scaled_pedal_position * 10.0;
+  int32_t torque_right = INVERTER_MAX_POSITIVE_TORQUE * scaled_pedal_position * 10.0;
+  torque_setpoints.torques[0] = torque_left;
+  torque_setpoints.torques[1] = torque_right;
+  torque_setpoints.torques[2] = torque_left;
+  torque_setpoints.torques[3] = torque_right;
 
     setpoints_publisher->publish(torque_setpoints);
-  }
 }
 
 int main(int argc, char** argv) {
