466726dc919dd0e64b3646e70de3d910fc567552
KozAAAAA <mateusz-koza@outlook.com>
2024-07-23 19:24:41 +0200

    Update generated code and modify code accordingly

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,7 +1,9 @@
 #include "putm_vcl_interfaces/msg/frontbox_driver_input.hpp"
 #include "putm_vcl_interfaces/msg/setpoints.hpp"
 #include "rclcpp/rclcpp.hpp"
+
 extern "C" {
+#include "read.h"
 #include "tv.h"
 }
 
@@ -16,20 +18,6 @@ class Controller : public rclcpp::Node {
   ~Controller();
 
  private:
-  static constexpr float INVERTER_MAX_TORQUE = 9.8;
-  struct TvValues {
-    double WheelSp_FL = 0.0;
-    double WheelSp_FR = 0.0;
-    double WheelSp_RL = 0.0;
-    double WheelSp_RR = 0.0;
-    double acc_pedal = 0.0;
-    double ax = 0.0;
-    double ay = 0.0;
-    double brake_pedal = 0.0;
-    double delta = 0.0;
-    double yaw_rate = 0.0;
-  };
-
   FrontboxDriverInput frontbox_driver_input;
 
   rclcpp::Publisher<Setpoints>::SharedPtr setpoints_publisher;
@@ -43,7 +31,6 @@ class Controller : public rclcpp::Node {
   inline int32_t convert_torque(double torque);
   void frontbox_driver_input_topic_callback(const FrontboxDriverInput msg);
 
-  void tv_set_values(const TvValues& tv_values);
   void control_loop();
 };
 
@@ -52,48 +39,38 @@ Controller::Controller()
       setpoints_publisher(this->create_publisher<Setpoints>("putm_vcl/setpoints", 1)),
       frontbox_driver_input_subscriber(this->create_subscription<FrontboxDriverInput>("putm_vcl/frontbox_driver_input", 1,
                                                                                       std::bind(&Controller::frontbox_driver_input_topic_callback, this, _1))),
-      control_loop_timer(this->create_wall_timer(10ms, std::bind(&Controller::control_loop, this)))  // TODO: Determine control loop frequency
-{
+      control_loop_timer(this->create_wall_timer(10ms, std::bind(&Controller::control_loop, this))) {
   tv_initialize();
+  read_inputs();
 }
 
 Controller::~Controller() { tv_terminate(); }
 
 void Controller::frontbox_driver_input_topic_callback(const FrontboxDriverInput msg) { frontbox_driver_input = msg; }
 
-void Controller::tv_set_values(const TvValues& tv_values) {
-  tv_P.WheelSp_FL_Value = tv_values.WheelSp_FL;
-  tv_P.WheelSp_FR_Value = tv_values.WheelSp_FR;
-  tv_P.WheelSp_RL_Value = tv_values.WheelSp_RL;
-  tv_P.WheelSp_RR_Value = tv_values.WheelSp_RR;
-  tv_P.acc_pedal_Value = tv_values.acc_pedal;
-  tv_P.ax_Value = tv_values.ax;
-  tv_P.ay_Value = tv_values.ay;
-  tv_P.brake_pedal_Value = tv_values.brake_pedal;
-  tv_P.delta_Value = tv_values.delta;
-  tv_P.yaw_rate_Value = tv_values.yaw_rate;
-}
-
 void Controller::control_loop() {
   if (rtmGetErrorStatus(tv_M) == (NULL) && !rtmGetStopRequested(tv_M)) {
-    TvValues tv_values;
-    tv_values.acc_pedal = convert_pedal_position(frontbox_driver_input.pedal_position);
-    tv_values.brake_pedal = convert_brake_pressure((frontbox_driver_input.brake_pressure_front + frontbox_driver_input.brake_pressure_rear) / 2);
-    tv_values.delta = convert_steering_wheel_position(frontbox_driver_input.steering_wheel_position);
-    tv_set_values(tv_values);
+    tv_P.acc_pedal_Value = convert_pedal_position(frontbox_driver_input.pedal_position);
+    tv_P.brake_pedal_Value = convert_brake_pressure((frontbox_driver_input.brake_pressure_front + frontbox_driver_input.brake_pressure_rear) / 2);
+    tv_P.delta_Value = convert_steering_wheel_position(frontbox_driver_input.steering_wheel_position);
 
     tv_step();
 
-    RCLCPP_INFO_STREAM(this->get_logger(), "Trq_FL_Value:" << tv_B.Trq_FL);
-    RCLCPP_INFO_STREAM(this->get_logger(), "Trq_FR_Value:" << tv_B.Trq_FR);
-    RCLCPP_INFO_STREAM(this->get_logger(), "Trq_RL_Value:" << tv_B.Trq_RL);
-    RCLCPP_INFO_STREAM(this->get_logger(), "Trq_RR_Value:" << tv_B.Trq_RR);
+    double torque_fl = tv_B.Trq_FL_scaled / tv_P.drive_ratio;
+    double torque_fr = tv_B.Trq_FR_scaled / tv_P.drive_ratio;
+    double torque_rl = tv_B.Trq_RL_scaled / tv_P.drive_ratio;
+    double torque_rr = tv_B.Trq_RR_scaled / tv_P.drive_ratio;
+
+    RCLCPP_INFO_STREAM(this->get_logger(), "FL: " << torque_fl);
+    RCLCPP_INFO_STREAM(this->get_logger(), "FR: " << torque_fr);
+    RCLCPP_INFO_STREAM(this->get_logger(), "RL: " << torque_rl);
+    RCLCPP_INFO_STREAM(this->get_logger(), "RR: " << torque_rr);
 
     auto setpoints = Setpoints();
-    setpoints.front_left.torque = convert_torque(tv_B.Trq_FL);
-    setpoints.front_right.torque = convert_torque(tv_B.Trq_FR);
-    setpoints.rear_left.torque = convert_torque(tv_B.Trq_RL);
-    setpoints.rear_right.torque = convert_torque(tv_B.Trq_RR);
+    setpoints.front_left.torque = convert_torque(torque_fl);
+    setpoints.front_right.torque = convert_torque(torque_fr);
+    setpoints.rear_left.torque = convert_torque(torque_rl);
+    setpoints.rear_right.torque = convert_torque(torque_rr);
 
     setpoints_publisher->publish(setpoints);
   } else {
@@ -102,8 +79,8 @@ void Controller::control_loop() {
 }
 
 inline double Controller::convert_pedal_position(int16_t pedal_position) {
-  static constexpr int32_t PEDAL_SCALER = 500;
-  return (((float)pedal_position) / PEDAL_SCALER);
+  static constexpr double PEDAL_SCALER = 500.0;
+  return (((double)pedal_position) / PEDAL_SCALER);
 }
 
 inline double Controller::convert_brake_pressure(int16_t brake_pressure) {
@@ -117,7 +94,7 @@ inline double Controller::convert_steering_wheel_position(int16_t steering_wheel
 }
 
 inline int32_t Controller::convert_torque(double torque) {
-  static constexpr float TORQUE_SCALER = 10.0;
+  static constexpr double TORQUE_SCALER = 10.0;
   return (int32_t)(torque * TORQUE_SCALER);
 }
 
