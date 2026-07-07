3b120cf6eebd785deb4aa7d3a1d636f325916813
Czarne-oczki <czarnocki.michal2002@gmail.com>
2026-07-07 15:56:53 +0200

    some changes idk

--- src/controller.cpp
+++ src/controller.cpp
@@ -302,12 +302,13 @@ void Controller::control_loop() {
 
     auto velocity_rear_left_error = velocity_set - (speed_rl * 2 * 3.1415 * 0.198 / 60);
     integral_rear_left += velocity_rear_left_error;
-    auto tau_final[1] = K_sc * velocity_rear_left_error + K_integral_sc * integral_rear_left;
+    auto tau_final[2] = K_sc * velocity_rear_left_error + K_integral_sc * integral_rear_left;
 
     auto velocity_rear_right_error = velocity_set - (speed_rr * 2 * 3.1415 * 0.198 / 60);
     integral_rear_right += velocity_rear_right_error;
-    auto tau_final[1] = K_sc * velocity_rear_right_error + K_integral_sc * integral_rear_right;
-
+    auto tau_final[3] = K_sc * velocity_rear_right_error + K_integral_sc * integral_rear_right;
+    
+    
   }
 
   if (pedal < 0.01) {
